8d9b9e248c8280d20ffe997aa10757e3911cf353
Rajiv Lochan Dash <razzivofficial@gmail.com>
2022-11-18 02:43:36 +0530

    MANY CHANGES BY RAJIV 18-11-22 AT 02:45

--- Genic Store Project/JS/signup.js
+++ Genic Store Project/JS/signup.js
@@ -1,21 +1,3 @@
-var faq = document.getElementsByClassName("faq-page");
-var i;
-
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
-}
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
---
+++ exe.js
@@ -0,0 +1,126 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+if (menu.classList.contains("active")) {
+  menu.classList.remove("active");
+  toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+} else {
+  menu.classList.add("active");
+  toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+}
+}
+
+/* Activate Submenu */
+function toggleItem() {
+if (this.classList.contains("submenu-active")) {
+  this.classList.remove("submenu-active");
+} else if (menu.querySelector(".submenu-active")) {
+  menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  this.classList.add("submenu-active");
+} else {
+  this.classList.add("submenu-active");
+}
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+if (menu.querySelector(".submenu-active")) {
+  let isClickInside = menu
+    .querySelector(".submenu-active")
+    .contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+if (item.querySelector(".submenu")) {
+  item.addEventListener("click", toggleItem, false);
+}
+item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
+
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+for (i = 0; i < slides.length; i++) 
+{
+  slides[i].style.display = "none";  
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}    
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+// dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
